c3dd064bdf7f2f6966c6e04eef33164f2af5bc22
Jerry <jerrynutt416@gmail.com>
2021-07-28 23:31:13 -0400

    grid changes

--- src/components/image.js
+++ src/components/image.js
@@ -2,13 +2,20 @@
 const Image = ({item,index}) =>{
     console.log(item.urls.full)
     return(
+        
        <div className="card">
+           <div className="shell">
+            <div className="popup-background"></div>
             <img src={item.urls.small} alt=""></img>
+            <div className="buttonBox"><button>One</button><button>Two</button><button>Three</button></div>
+            
+            </div>
 
 
 
 
        </div>
+       
     )
 }
 export default Image
