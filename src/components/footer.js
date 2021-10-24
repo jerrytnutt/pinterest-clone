85a8f615c88177e07f318268b6e722cc62954608
Jerry <jerrynutt416@gmail.com>
2021-10-24 19:50:23 -0400

    Update

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,16 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   useEffect(() => {}, []);
 
-  return <div className="footer">hhh</div>;
+  return (
+    <div className="footer">
+      <i
+        onClick={() => {
+          window.open("https://github.com/jerrytnutt/pinterest-clone");
+        }}
+        class="fa fa-github"
+        aria-hidden="true"
+      ></i>
+    </div>
+  );
 };
 export default Footer;
